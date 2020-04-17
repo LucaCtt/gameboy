e0d366124bd752824f99f3d358970ac23b4e47a5
Luca Cotti <lucacotti@outlook.com>
2020-04-17 17:47:30 +0200

    Add basic mbc1 implementation
    The mbc1 does not yet include ROM/RAM bank selection.

--- cart/mbc1.go
+++ cart/mbc1.go
@@ -1,70 +1,105 @@
 package cart
 
-import "github.com/lucactt/gameboy/mem"
+import (
+	"fmt"
 
+	"github.com/lucactt/gameboy/mem"
+	"github.com/lucactt/gameboy/util/errors"
+)
+
+// Memory address ranges
+const (
+	mbc1ROMBank0Start  uint16 = 0x0000
+	mbc1ROMBank0End    uint16 = 0x3FFF
+	mbc1SwitchROMStart uint16 = 0x4000
+	mbc1SwitchROMEnd   uint16 = 0x7FFF
+	mbc1SwitchRAMStart uint16 = 0xA000
+	mbc1SwitchRAMEnd   uint16 = 0xBFFF
+	mbc1RAMEnableStart uint16 = 0x0000
+	mbc1RAMEnableEnd   uint16 = 0x1FFF
+	mbc1ROMBankStart   uint16 = 0x2000
+	mbc1ROMBankEnd     uint16 = 0x3FFF
+	mbc1RAMBankStart   uint16 = 0x4000
+	mbc1RAMBankEnd     uint16 = 0x5FFF
+	mbc1ModeStart      uint16 = 0x6000
+	mbc1ModeEnd        uint16 = 0x7FFF
+
+	mbc1EnableRAMValue byte = 0x0A
+)
+
+// MBC1 implements an MBC1 cartridge controller.
 type MBC1 struct {
-	rom                *mem.ROM
-	ram                *mem.RAM
-	romBanks, ramBanks int
-	ramEnabled         bool
-	isROMBanking       bool
-	romBank, ramBank   int
+	mem          mem.Mem
+	isROMBanking bool
+	isRAMEnabled bool
 }
 
-func NewMBC1(rom *mem.ROM, romBanks, ramBanks int) *MBC1 {
-	ram := mem.NewRAM(uint16(0x2000 * ramBanks))
-	return &MBC1{rom, ram, romBanks, ramBanks, false, true, 1, 1}
+// NewMBC1 creates a new MBC1 controller from the given ROM and RAM.
+//
+// The ROM must at least be big enough to contain the addresses until 0x7FFF.
+//
+// If the RAM is not nil, it must at least be big enough to accept the addresses
+// between 0xA000 and 0xBFFF.
+func NewMBC1(rom *mem.ROM, ram *mem.RAM) (*MBC1, error) {
+	if !rom.Accepts(mbc1SwitchROMEnd) {
+		return nil, errors.E("ROM size insufficient", errors.Cartridge)
+	}
+
+	if ram == nil {
+		return &MBC1{rom, true, false}, nil
+	}
+
+	if !ram.Accepts(mbc1SwitchRAMEnd - mbc1SwitchRAMStart) {
+		return nil, errors.E("RAM size insufficient", errors.Cartridge)
+	}
+
+	mmu := &mem.MMU{}
+	mmu.AddMem(mbc1ROMBank0Start, rom)
+	mmu.AddMem(mbc1SwitchRAMStart, ram)
+
+	return &MBC1{mmu, false, false}, nil
 }
 
+// GetByte returns the byte at the given address, which
+// can be read from the ROM or from the RAM, if it exists.
 func (ctr *MBC1) GetByte(addr uint16) (byte, error) {
-	if isRAM(addr) {
+	if !ctr.Accepts(addr) {
+		return 0, errors.E(fmt.Sprintf("MBC1 controller does not accepts addr %d", addr))
+	}
+
+	if addr >= mbc1SwitchRAMStart && addr <= mbc1SwitchRAMEnd && !ctr.isRAMEnabled {
 		return 0xFF, nil
 	}
 
-	return getByte(ctr.rom, addr), nil
+	byte, err := ctr.mem.GetByte(addr)
+	if err != nil {
+		panic(err)
+	}
+
+	return byte, nil
 }
 
+// SetByte does nothing if the addr points to the ROM,
+// or sets the byte to the given value if it points to RAM.
 func (ctr *MBC1) SetByte(addr uint16, value byte) error {
-	switch {
-	case addr >= 0x000 && addr <= 0x1FFF:
-		ctr.ramEnabled = (value & 0x0F) == 0x0A
-
-	case addr >= 0x2000 && addr <= 0x3FFF:
-		bank := (ctr.romBank & 0x60) | (int(value) & 0x1F)
-		ctr.romBank = bank
-
-	case addr >= 0x4000 && addr <= 0x5FFF:
-		if ctr.isROMBanking {
-			bank := ctr.romBank&0x1F | ((int(value) & 0x03) << 5)
-			ctr.romBank = bank
-		} else {
-			bank := int(value) & 0x03
-			ctr.ramBank = bank
-		}
-
-	case addr >= 0x6000 && addr <= 0x7FFF:
-		ctr.isROMBanking = (value == 0x00)
-
-	case addr >= 0xA000 && addr < 0xC000 && ctr.ramEnabled:
-		ramAddr := ctr.ramAddr(addr)
-		setByte(ctr.ram, ramAddr, value)
+	if !ctr.Accepts(addr) {
+		return errors.E(fmt.Sprintf("MBC1 controller does not accepts addr %d", addr))
 	}
+
+	if addr >= mbc1RAMEnableStart && addr <= mbc1RAMEnableEnd {
+		ctr.isRAMEnabled = (value == mbc1EnableRAMValue)
+	}
+
+	err := ctr.mem.SetByte(addr, value)
+	if err != nil {
+		panic(err)
+	}
+
 	return nil
 }
 
+// Accepts returns true if the address is included in the ROM
+// or in the RAM, false otherwise.
 func (ctr *MBC1) Accepts(addr uint16) bool {
-	return (addr >= 0x0000 && addr < 0x8000) ||
-		(addr >= 0xA000 && addr < 0xC000)
-}
-
-func (ctr *MBC1) ramAddr(addr uint16) uint16 {
-	if ctr.isROMBanking {
-		return addr - 0xA000
-	}
-
-	return uint16(ctr.ramBank%ctr.ramBanks)*0x2000 + (addr - 0xa000)
-}
-
-func isRAM(addr uint16) bool {
-	return addr >= 0xA000 && addr < 0xC000
+	return ctr.mem.Accepts(addr)
 }
---
+++ cart/mbc1_test.go
@@ -0,0 +1,176 @@
+package cart
+
+import (
+	"testing"
+
+	"github.com/lucactt/gameboy/mem"
+	"github.com/lucactt/gameboy/util/assert"
+)
+
+const minRAMSize = mbc1SwitchRAMEnd + 1 - mbc1SwitchRAMStart
+
+func TestNewMBC1(t *testing.T) {
+	t.Run("ROM is too small", func(t *testing.T) {
+		bytes := make([]byte, 0)
+		_, err := NewMBC1(mem.NewROM(bytes), nil)
+		assert.Err(t, err, true)
+	})
+
+	t.Run("ROM is big enough", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		_, err := NewMBC1(mem.NewROM(bytes), nil)
+		assert.Err(t, err, false)
+	})
+
+	t.Run("RAM is too small", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		_, err := NewMBC1(mem.NewROM(bytes), mem.NewRAM(0))
+		assert.Err(t, err, true)
+	})
+
+	t.Run("RAM is big enough", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		_, err := NewMBC1(mem.NewROM(bytes), mem.NewRAM(minRAMSize))
+		assert.Err(t, err, false)
+	})
+}
+
+func TestMBC1_GetByte(t *testing.T) {
+	t.Run("Unswitchable ROM bank", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		bytes[mbc1ROMBank0Start] = 0x11
+
+		ctr, _ := NewMBC1(mem.NewROM(bytes), nil)
+
+		got, err := ctr.GetByte(mbc1ROMBank0Start)
+		assert.Err(t, err, false)
+		assert.Equal(t, got, byte(0x11))
+	})
+
+	t.Run("RAM bank, RAM enabled", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), mem.NewRAM(minRAMSize))
+
+		// Make sure RAM is enabled
+		ctr.SetByte(mbc1RAMEnableStart, mbc1EnableRAMValue)
+		ctr.SetByte(mbc1SwitchRAMEnd, 0x11)
+
+		got, err := ctr.GetByte(mbc1SwitchRAMEnd)
+		assert.Err(t, err, false)
+		assert.Equal(t, got, byte(0x11))
+	})
+
+	t.Run("RAM bank, RAM disabled", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), mem.NewRAM(minRAMSize))
+
+		// Make sure RAM is disabled
+		ctr.SetByte(mbc1RAMEnableStart, 0x00)
+
+		got, err := ctr.GetByte(mbc1SwitchRAMEnd)
+		assert.Err(t, err, false)
+		assert.Equal(t, got, byte(0xFF))
+	})
+
+	t.Run("RAM bank, but no RAM", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), nil)
+
+		ctr.SetByte(mbc1SwitchRAMEnd, 0x11)
+
+		_, err := ctr.GetByte(mbc1SwitchRAMEnd)
+		assert.Err(t, err, true)
+	})
+}
+
+func TestMBC1_SetByte(t *testing.T) {
+	t.Run("Disable RAM", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), mem.NewRAM(minRAMSize))
+
+		ctr.SetByte(mbc1RAMEnableStart, 0x00)
+
+		got, err := ctr.GetByte(mbc1SwitchRAMStart)
+		assert.Err(t, err, false)
+		assert.Equal(t, got, byte(0xFF))
+	})
+
+	t.Run("Enable RAM", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+
+		ctr, _ := NewMBC1(mem.NewROM(bytes), mem.NewRAM(minRAMSize))
+		ctr.SetByte(mbc1RAMEnableStart, mbc1EnableRAMValue)
+		ctr.SetByte(mbc1SwitchRAMStart, 0x11)
+
+		got, err := ctr.GetByte(mbc1SwitchRAMStart)
+		assert.Err(t, err, false)
+		assert.Equal(t, got, byte(0x11))
+	})
+
+	t.Run("Enable ROM banking", func(t *testing.T) {
+	})
+
+	t.Run("ROM banking by default", func(t *testing.T) {
+	})
+
+	t.Run("Enable RAM banking", func(t *testing.T) {
+	})
+
+	t.Run("ROM banking allows access only to RAM bank 0 ", func(t *testing.T) {
+	})
+
+	t.Run("RAM banking allows access only to ROM banks 0x01-0x1F", func(t *testing.T) {
+	})
+
+	t.Run("Switch ROM bank", func(t *testing.T) {
+	})
+
+	t.Run("Switch RAM bank", func(t *testing.T) {
+	})
+}
+
+func TestMBC1_Accepts(t *testing.T) {
+	t.Run("RAM address", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), mem.NewRAM(minRAMSize))
+
+		got := ctr.Accepts(mbc1SwitchRAMStart)
+		assert.Equal(t, got, true)
+
+		got = ctr.Accepts(mbc1SwitchRAMEnd)
+		assert.Equal(t, got, true)
+	})
+
+	t.Run("RAM address, but no RAM", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), nil)
+
+		got := ctr.Accepts(mbc1SwitchRAMStart)
+		assert.Equal(t, got, false)
+
+		got = ctr.Accepts(mbc1SwitchRAMEnd)
+		assert.Equal(t, got, false)
+	})
+
+	t.Run("ROM address", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), nil)
+
+		got := ctr.Accepts(mbc1ROMBank0Start)
+		assert.Equal(t, got, true)
+
+		got = ctr.Accepts(mbc1ROMBank0End)
+		assert.Equal(t, got, true)
+	})
+
+	t.Run("Outside mem", func(t *testing.T) {
+		bytes := make([]byte, mbc1SwitchROMEnd+1)
+		ctr, _ := NewMBC1(mem.NewROM(bytes), nil)
+
+		got := ctr.Accepts(0xFFFF)
+		assert.Equal(t, got, false)
+
+		got = ctr.Accepts(mbc1ModeEnd + 1)
+		assert.Equal(t, got, false)
+	})
+}
